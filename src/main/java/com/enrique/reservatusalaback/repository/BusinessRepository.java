696b1dc0421c667c831be1e02854915cf82bcba4
Enrique Gómez <egomez@profile.es>
2022-11-30 12:13:41 +0100

    all repositories done

---
+++ src/main/java/com/enrique/reservatusalaback/repository/BusinessRepository.java
@@ -0,0 +1,8 @@
+package com.enrique.reservatusalaback.repository;
+
+import com.enrique.reservatusalaback.model.Business;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BusinessRepository extends JpaRepository<Business, Integer> {
+
+}
---
+++ src/main/java/com/enrique/reservatusalaback/repository/RoomRepository.java
@@ -0,0 +1,8 @@
+package com.enrique.reservatusalaback.repository;
+
+import com.enrique.reservatusalaback.model.Room;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoomRepository extends JpaRepository<Room, Integer> {
+
+}
