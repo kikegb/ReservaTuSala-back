1292cef1f7666e62d08c4e9be917e3786f5ab382
Enrique Gómez <egomez@profile.es>
2022-11-30 11:45:10 +0100

    base project

---
+++ src/main/java/com/enrique/reservatusalaback/ReservatusalaBackApplication.java
@@ -0,0 +1,13 @@
+package com.enrique.reservatusalaback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservatusalaBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservatusalaBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/enrique/reservatusalaback/ReservatusalaBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enrique.reservatusalaback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservatusalaBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
