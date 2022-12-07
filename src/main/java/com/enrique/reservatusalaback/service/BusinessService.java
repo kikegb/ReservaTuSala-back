0083f22018e22e7f0299efa849bddc803948f404
Enrique Gómez <egomez@profile.es>
2022-12-07 12:09:58 +0100

    some business service methods implemented

--- src/main/java/com/enrique/reservatusalaback/service/BusinessService.java
+++ src/main/java/com/enrique/reservatusalaback/service/BusinessService.java
@@ -1,5 +1,13 @@
 package com.enrique.reservatusalaback.service;
 
-public interface BusinessService {
+import com.enrique.reservatusalaback.model.Business;
 
+import java.util.List;
+
+public interface BusinessService {
+    Business add(Business business);
+    List<Business> findAll();
+    Business findById(Long id);
+    Business update(Business business);
+    int deleteById(Long id);
 }
--- src/main/java/com/enrique/reservatusalaback/service/impl/BusinessServiceImpl.java
+++ src/main/java/com/enrique/reservatusalaback/service/impl/BusinessServiceImpl.java
@@ -1,11 +1,15 @@
 package com.enrique.reservatusalaback.service.impl;
 
+import com.enrique.reservatusalaback.model.Business;
 import com.enrique.reservatusalaback.repository.BusinessRepository;
 import com.enrique.reservatusalaback.service.BusinessService;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @AllArgsConstructor
@@ -14,5 +18,37 @@ public class BusinessServiceImpl implements BusinessService {
 
     @Autowired
     private BusinessRepository businessRepository;
+
+    @Override
+    public Business add(final Business business) {
+        return businessRepository.save(business);
+    }
+
+    @Override
+    public List<Business> findAll() {
+        return businessRepository.findAll();
+    }
+
+    @Override
+    public Business findById(final Long id) {
+        return businessRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Business update(final Business business) {
+        return businessRepository.save(business);
+    }
+
+    @Override
+    public int deleteById(final Long id) {
+        Optional<Business> result = businessRepository.findById(id);
+        if (result.isPresent()) {
+            Business business = result.get();
+            business.setDeleted(true);
+            businessRepository.save(business);
+            return 1;
+        }
+        return -1;
+    }
 
 }
