b05bec958533e663271e9985b51c71c69be13a89
Enrique Gómez <egomez@profile.es>
2022-12-07 12:10:20 +0100

    some customer service methods implemented

--- src/main/java/com/enrique/reservatusalaback/service/CustomerService.java
+++ src/main/java/com/enrique/reservatusalaback/service/CustomerService.java
@@ -1,5 +1,13 @@
 package com.enrique.reservatusalaback.service;
 
+import com.enrique.reservatusalaback.model.Customer;
+
+import java.util.List;
+
 public interface CustomerService {
-    //int softDelete(Integer id);
+    Customer add(Customer customer);
+    List<Customer> findAll();
+    Customer findById(Long id);
+    Customer update(Customer customer);
+    int deleteById(Long id);
 }
--- src/main/java/com/enrique/reservatusalaback/service/impl/CustomerServiceImpl.java
+++ src/main/java/com/enrique/reservatusalaback/service/impl/CustomerServiceImpl.java
@@ -1,11 +1,15 @@
 package com.enrique.reservatusalaback.service.impl;
 
+import com.enrique.reservatusalaback.model.Customer;
 import com.enrique.reservatusalaback.repository.CustomerRepository;
 import com.enrique.reservatusalaback.service.CustomerService;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @AllArgsConstructor
@@ -15,10 +19,35 @@ public class CustomerServiceImpl implements CustomerService {
     @Autowired
     private CustomerRepository customerRepository;
 
-    /*
     @Override
-    public int softDelete(Integer id) {
-        return 0;
+    public Customer add(final Customer customer) {
+        return customerRepository.save(customer);
     }
-    */
+
+    @Override
+    public List<Customer> findAll() {
+        return customerRepository.findAll();
+    }
+
+    @Override
+    public Customer findById(final Long id) {
+        return customerRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Customer update(final Customer customer) {
+        return customerRepository.save(customer);
+    }
+
+    @Override
+    public int deleteById(final Long id) {
+        Optional<Customer> result = customerRepository.findById(id);
+        if (result.isPresent()) {
+            Customer customer = result.get();
+            customer.setDeleted(true);
+            customerRepository.save(customer);
+            return 1;
+        }
+        return -1;
+    }
 }
