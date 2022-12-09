59488b570cc9c8e18576eb5cf9355c43203f20bd
Enrique Gómez <egomez@profile.es>
2022-12-09 11:12:50 +0100

    material service done

--- src/main/java/com/enrique/reservatusalaback/service/MaterialService.java
+++ src/main/java/com/enrique/reservatusalaback/service/MaterialService.java
@@ -1,5 +1,14 @@
 package com.enrique.reservatusalaback.service;
 
+import com.enrique.reservatusalaback.model.Material;
+
+import java.util.List;
+
 public interface MaterialService {
+    Material add(Long roomId, Material material);
+    List<Material> findAll();
+    Material findById(Long id);
+    Material update(Material material);
+    int deleteById(Long id);
 
 }
--- src/main/java/com/enrique/reservatusalaback/service/impl/MaterialServiceImpl.java
+++ src/main/java/com/enrique/reservatusalaback/service/impl/MaterialServiceImpl.java
@@ -1,18 +1,62 @@
 package com.enrique.reservatusalaback.service.impl;
 
+import com.enrique.reservatusalaback.model.Material;
 import com.enrique.reservatusalaback.repository.MaterialRepository;
 import com.enrique.reservatusalaback.service.MaterialService;
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
+import com.enrique.reservatusalaback.service.RoomService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
-@AllArgsConstructor
-@NoArgsConstructor
 public class MaterialServiceImpl implements MaterialService {
 
-    @Autowired
-    private MaterialRepository materialRepository;
+    private final MaterialRepository materialRepository;
+    private final RoomService roomService;
 
+    @Autowired
+    public MaterialServiceImpl(MaterialRepository materialRepository, RoomService roomService) {
+        this.materialRepository = materialRepository;
+        this.roomService = roomService;
+    }
+
+    @Override
+    public Material add(Long roomId, Material material) {
+        Material newMaterial = materialRepository.save(material);
+        int result = roomService.addMaterial(roomId, material);
+        if (result < 0) {
+            materialRepository.deleteById(newMaterial.getId());
+            return null;
+        }
+        return newMaterial;
+    }
+
+    @Override
+    public List<Material> findAll() {
+        return materialRepository.findAll();
+    }
+
+    @Override
+    public Material findById(Long id) {
+        return materialRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Material update(Material material) {
+        return materialRepository.save(material);
+    }
+
+    @Override
+    public int deleteById(Long id) {
+        Optional<Material> result = materialRepository.findById(id);
+        if (result.isPresent()) {
+            Material material = result.get();
+            material.setDeleted(true);
+            materialRepository.save(material);
+            return 1;
+        }
+        return -1;
+    }
 }
