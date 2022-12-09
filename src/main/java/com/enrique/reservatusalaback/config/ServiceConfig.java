c455811121a50cb632dc8fa4158484928a67c44a
Enrique Gómez <egomez@profile.es>
2022-12-09 12:20:07 +0100

    service config class done

--- src/main/java/com/enrique/reservatusalaback/config/ServiceConfig.java
+++ src/main/java/com/enrique/reservatusalaback/config/ServiceConfig.java
@@ -3,13 +3,24 @@ package com.enrique.reservatusalaback.config;
 import com.enrique.reservatusalaback.repository.BusinessRepository;
 import com.enrique.reservatusalaback.repository.CustomerRepository;
 import com.enrique.reservatusalaback.repository.LocationRepository;
+import com.enrique.reservatusalaback.repository.MaterialRepository;
+import com.enrique.reservatusalaback.repository.OperationRepository;
+import com.enrique.reservatusalaback.repository.RoomRepository;
+import com.enrique.reservatusalaback.repository.ScheduleRepository;
 import com.enrique.reservatusalaback.service.BusinessService;
 import com.enrique.reservatusalaback.service.CustomerService;
 import com.enrique.reservatusalaback.service.LocationService;
+import com.enrique.reservatusalaback.service.MaterialService;
+import com.enrique.reservatusalaback.service.OperationService;
+import com.enrique.reservatusalaback.service.RoomService;
+import com.enrique.reservatusalaback.service.ScheduleService;
 import com.enrique.reservatusalaback.service.impl.BusinessServiceImpl;
 import com.enrique.reservatusalaback.service.impl.CustomerServiceImpl;
 import com.enrique.reservatusalaback.service.impl.LocationServiceImpl;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.enrique.reservatusalaback.service.impl.MaterialServiceImpl;
+import com.enrique.reservatusalaback.service.impl.OperationServiceImpl;
+import com.enrique.reservatusalaback.service.impl.RoomServiceImpl;
+import com.enrique.reservatusalaback.service.impl.ScheduleServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -18,26 +29,41 @@ import org.springframework.context.annotation.Configuration;
 @ComponentScan("com.enrique.reservatusalaback")
 public class ServiceConfig {
 
-    @Autowired
-    private BusinessRepository businessRepository;
-    @Autowired
-    private CustomerRepository customerRepository;
-    @Autowired
-    private LocationRepository locationRepository;
-
     @Bean
-    public BusinessService businessService() {
+    public BusinessService businessService(BusinessRepository businessRepository) {
         return new BusinessServiceImpl(businessRepository);
     }
 
     @Bean
-    public CustomerService customerService() {
+    public CustomerService customerService(CustomerRepository customerRepository) {
         return new CustomerServiceImpl(customerRepository);
     }
 
     @Bean
-    public LocationService locationService() {
+    public RoomService roomService(RoomRepository roomRepository, BusinessService businessService) {
+        return new RoomServiceImpl(roomRepository, businessService);
+    }
+
+    @Bean
+    public LocationService locationService(LocationRepository locationRepository) {
         return new LocationServiceImpl(locationRepository);
+    }
+
+    @Bean
+    public MaterialService materialService(MaterialRepository materialRepository, RoomService roomService) {
+        return new MaterialServiceImpl(materialRepository, roomService);
+    }
+
+    @Bean
+    public ScheduleService scheduleService(ScheduleRepository scheduleRepository, RoomService roomService) {
+        return new ScheduleServiceImpl(scheduleRepository, roomService);
+    }
+
+    @Bean
+    public OperationService operationService(OperationRepository operationRepository, BusinessService businessService,
+                                             CustomerService customerService, RoomService roomService)
+    {
+        return new OperationServiceImpl(operationRepository, businessService, customerService, roomService);
     }
 
 }
