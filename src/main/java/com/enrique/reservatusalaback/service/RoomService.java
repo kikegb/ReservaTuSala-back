6235d9ac2dac680554e79c44e3349d3a94663fae
Enrique Gómez <egomez@profile.es>
2022-12-09 10:59:55 +0100

    room service done

--- src/main/java/com/enrique/reservatusalaback/service/RoomService.java
+++ src/main/java/com/enrique/reservatusalaback/service/RoomService.java
@@ -1,4 +1,19 @@
 package com.enrique.reservatusalaback.service;
 
+import com.enrique.reservatusalaback.model.Material;
+import com.enrique.reservatusalaback.model.Operation;
+import com.enrique.reservatusalaback.model.Room;
+import com.enrique.reservatusalaback.model.Schedule;
+
+import java.util.List;
+
 public interface RoomService {
+    Room add(Long businessId, Room room);
+    List<Room> findAll();
+    Room findById(Long id);
+    Room update(Room room);
+    int deleteById(Long id);
+    int addOperation(Long id, Operation operation);
+    int addSchedule(Long id, Schedule schedule);
+    int addMaterial(Long id, Material material);
 }
--- src/main/java/com/enrique/reservatusalaback/service/impl/RoomServiceImpl.java
+++ src/main/java/com/enrique/reservatusalaback/service/impl/RoomServiceImpl.java
@@ -1,18 +1,104 @@
 package com.enrique.reservatusalaback.service.impl;
 
+import com.enrique.reservatusalaback.model.Material;
+import com.enrique.reservatusalaback.model.Operation;
+import com.enrique.reservatusalaback.model.Room;
+import com.enrique.reservatusalaback.model.Schedule;
 import com.enrique.reservatusalaback.repository.RoomRepository;
+import com.enrique.reservatusalaback.service.BusinessService;
 import com.enrique.reservatusalaback.service.RoomService;
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
-@AllArgsConstructor
-@NoArgsConstructor
 public class RoomServiceImpl implements RoomService {
 
-    @Autowired
-    private RoomRepository roomRepository;
+    private final RoomRepository roomRepository;
+    private final BusinessService businessService;
 
+    @Autowired
+    public RoomServiceImpl(RoomRepository roomRepository, BusinessService businessService) {
+        this.roomRepository = roomRepository;
+        this.businessService = businessService;
+    }
+
+    @Override
+    public Room add(Long businessId, Room room) {
+        Room newRoom = roomRepository.save(room);
+        int result = businessService.addRoom(businessId, newRoom);
+        if (result < 0) {
+            roomRepository.deleteById(newRoom.getId());
+            return null;
+        }
+        return newRoom;
+    }
+
+    @Override
+    public List<Room> findAll() {
+        return roomRepository.findAll();
+    }
+
+    @Override
+    public Room findById(Long id) {
+        return roomRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Room update(Room room) {
+        if (roomRepository.existsById(room.getId())) {
+            return roomRepository.save(room);
+        }
+        return null;
+    }
+
+    @Override
+    public int deleteById(Long id) {
+        Optional<Room> result = roomRepository.findById(id);
+        if (result.isPresent()) {
+            Room room = result.get();
+            room.setDeleted(true);
+            roomRepository.save(room);
+            return 0;
+        }
+        return -1;
+    }
+
+    @Override
+    public int addOperation(Long id, Operation operation) {
+        Optional<Room> result = roomRepository.findById(id);
+        if (result.isPresent()) {
+            Room room = result.get();
+            room.getOperations().add(operation);
+            roomRepository.save(room);
+            return 0;
+        }
+        return -1;
+    }
+
+    @Override
+    public int addSchedule(Long id, Schedule schedule) {
+        Optional<Room> result = roomRepository.findById(id);
+        if (result.isPresent()) {
+            Room room = result.get();
+            room.getSchedule().add(schedule);
+            roomRepository.save(room);
+            return 0;
+        }
+        return -1;
+    }
+
+    @Override
+    public int addMaterial(Long id, Material material) {
+        Optional<Room> result = roomRepository.findById(id);
+        if (result.isPresent()) {
+            Room room = result.get();
+            room.getMaterials().add(material);
+            roomRepository.save(room);
+            return 0;
+        }
+        return -1;
+    }
 }
