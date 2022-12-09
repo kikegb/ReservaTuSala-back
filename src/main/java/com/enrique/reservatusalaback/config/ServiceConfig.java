e21a139175c04a9bbced9112c10118d6d160a1b3
Enrique Gómez <egomez@profile.es>
2022-12-09 09:53:15 +0100

    service config class: location bean

--- src/main/java/com/enrique/reservatusalaback/config/ServiceConfig.java
+++ src/main/java/com/enrique/reservatusalaback/config/ServiceConfig.java
@@ -2,10 +2,13 @@ package com.enrique.reservatusalaback.config;
 
 import com.enrique.reservatusalaback.repository.BusinessRepository;
 import com.enrique.reservatusalaback.repository.CustomerRepository;
+import com.enrique.reservatusalaback.repository.LocationRepository;
 import com.enrique.reservatusalaback.service.BusinessService;
 import com.enrique.reservatusalaback.service.CustomerService;
+import com.enrique.reservatusalaback.service.LocationService;
 import com.enrique.reservatusalaback.service.impl.BusinessServiceImpl;
 import com.enrique.reservatusalaback.service.impl.CustomerServiceImpl;
+import com.enrique.reservatusalaback.service.impl.LocationServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -19,6 +22,8 @@ public class ServiceConfig {
     private BusinessRepository businessRepository;
     @Autowired
     private CustomerRepository customerRepository;
+    @Autowired
+    private LocationRepository locationRepository;
 
     @Bean
     public BusinessService businessService() {
@@ -26,8 +31,13 @@ public class ServiceConfig {
     }
 
     @Bean
-    CustomerService customerService() {
+    public CustomerService customerService() {
         return new CustomerServiceImpl(customerRepository);
+    }
+
+    @Bean
+    public LocationService locationService() {
+        return new LocationServiceImpl(locationRepository);
     }
 
 }
