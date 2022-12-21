e23c0e1670609877216e44fd667d7ce18e902e47
Enrique Gómez <egomez@profile.es>
2022-12-21 12:10:28 +0100

    business controller tests done

--- src/main/java/com/enrique/reservatusalaback/controller/ResponseCode.java
+++ src/main/java/com/enrique/reservatusalaback/controller/ResponseCode.java
@@ -6,7 +6,7 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 public enum ResponseCode {
     OK(0, "OK"),
     NOT_FOUND_ID(1, "Invalid ID: ID not found in database."),
-    ALREADY_EXISTENT_USER(3, "User conflict: There is already a user with that email o cif/nif.");
+    ALREADY_EXISTENT_USER(2, "User conflict: There is already a user with that email or cif/nif.");
 
     public final int code;
     public final String description;
