fa65ec5927ffc7c89859d1e402a7351a52ba753c
Enrique Gómez <egomez@profile.es>
2022-12-27 15:38:35 +0100

    use openapi documentation with swagger + remove controller config class to use @RestController to make springdoc work

--- src/main/java/com/enrique/reservatusalaback/controller/CustomerController.java
+++ src/main/java/com/enrique/reservatusalaback/controller/CustomerController.java
@@ -13,9 +13,11 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@RestController
 @RequestMapping("/customer")
 @RequiredArgsConstructor
 public class CustomerController {
--- src/main/java/com/enrique/reservatusalaback/controller/MaterialController.java
+++ src/main/java/com/enrique/reservatusalaback/controller/MaterialController.java
@@ -12,9 +12,11 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@RestController
 @RequestMapping("/material")
 @RequiredArgsConstructor
 public class MaterialController {
