4853e1d100739ac0d128f89b95a9084aab686240
Enrique Gómez <egomez@profile.es>
2022-12-28 10:43:19 +0100

    validate not null input parameters on business controller

--- src/main/java/com/enrique/reservatusalaback/controller/BusinessController.java
+++ src/main/java/com/enrique/reservatusalaback/controller/BusinessController.java
@@ -27,6 +27,9 @@ public class BusinessController {
 
     @PostMapping
     public ResponseEntity<?> add(@RequestBody Business business) {
+        if (business == null) {
+            return new ResponseEntity<>(ResponseCode.NULL_OBJECT, HttpStatus.BAD_REQUEST);
+        }
         Business addedBusiness = businessService.add(business);
         if (addedBusiness == null) {
             return new ResponseEntity<>(ResponseCode.ALREADY_EXISTENT_USER, HttpStatus.CONFLICT);
@@ -41,6 +44,9 @@ public class BusinessController {
 
     @GetMapping("/findById")
     public ResponseEntity<?> findById(@RequestParam Long id) {
+        if (id == null) {
+            return new ResponseEntity<>(ResponseCode.NULL_ID, HttpStatus.BAD_REQUEST);
+        }
         Business business = businessService.findById(id);
         if (business == null){
             return new ResponseEntity<>(ResponseCode.NOT_FOUND_ID, HttpStatus.NOT_FOUND);
@@ -50,6 +56,9 @@ public class BusinessController {
 
     @PutMapping
     public ResponseEntity<?> update(@RequestBody Business business) {
+        if (business == null) {
+            return new ResponseEntity<>(ResponseCode.NULL_OBJECT, HttpStatus.BAD_REQUEST);
+        }
         Business updatedBusiness = businessService.update(business);
         if (updatedBusiness == null) {
             return new ResponseEntity<>(ResponseCode.NOT_FOUND_ID, HttpStatus.NOT_FOUND);
@@ -59,6 +68,9 @@ public class BusinessController {
 
     @DeleteMapping
     public ResponseEntity<ResponseCode> deleteById(@RequestParam Long id) {
+        if (id == null) {
+            return new ResponseEntity<>(ResponseCode.NULL_ID, HttpStatus.BAD_REQUEST);
+        }
         if (businessService.deleteById(id) > 0) {
             return new ResponseEntity<>(ResponseCode.NOT_FOUND_ID, HttpStatus.NOT_FOUND);
         } else {
@@ -68,6 +80,12 @@ public class BusinessController {
 
     @PostMapping("/addRoom")
     public ResponseEntity<?> addRoom(@RequestParam Long id, @RequestBody Room room) {
+        if (id == null) {
+            return new ResponseEntity<>(ResponseCode.NULL_ID, HttpStatus.BAD_REQUEST);
+        }
+        if (room == null) {
+            return new ResponseEntity<>(ResponseCode.NULL_OBJECT, HttpStatus.BAD_REQUEST);
+        }
         Room addedRoom = businessService.addRoom(id, room);
         if (addedRoom == null) {
             return new ResponseEntity<>(ResponseCode.NOT_FOUND_ID, HttpStatus.NOT_FOUND);
@@ -77,6 +95,12 @@ public class BusinessController {
 
     @PostMapping("/addOperation")
     public ResponseEntity<?> addOperation(@RequestParam Long id, @RequestBody Operation operation) {
+        if (id == null) {
+            return new ResponseEntity<>(ResponseCode.NULL_ID, HttpStatus.BAD_REQUEST);
+        }
+        if (operation == null) {
+            return new ResponseEntity<>(ResponseCode.NULL_OBJECT, HttpStatus.BAD_REQUEST);
+        }
         Operation addedOperation = businessService.addOperation(id, operation);
         if (addedOperation == null) {
             return new ResponseEntity<>(ResponseCode.NOT_FOUND_ID, HttpStatus.NOT_FOUND);
--- src/main/java/com/enrique/reservatusalaback/controller/ResponseCode.java
+++ src/main/java/com/enrique/reservatusalaback/controller/ResponseCode.java
@@ -6,7 +6,9 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 public enum ResponseCode {
     OK(0, "OK"),
     NOT_FOUND_ID(1, "Invalid ID: ID not found in database."),
-    ALREADY_EXISTENT_USER(2, "User conflict: There is already a user with that email or cif/nif.");
+    ALREADY_EXISTENT_USER(2, "User conflict: There is already a user with that email or cif/nif."),
+    NULL_OBJECT(3, "Bad request: The provided object is empty."),
+    NULL_ID(4, "Bad request: Null ID. A value is required.");
 
     public final int code;
     public final String description;
